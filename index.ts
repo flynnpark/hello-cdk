aa55e09e411f01baf03dfd14fc46b6d5b231e987
Flynn <wphestiraid@gmail.com>
2019-04-20 21:15:10 +0900

    간단한 AWS Lambda 함수 연결하는 템플릿으로 수정

--- index.ts
+++ index.ts
@@ -1,31 +1,24 @@
 import cdk = require('@aws-cdk/cdk');
-import ec2 = require('@aws-cdk/aws-ec2');
-import ecs = require('@aws-cdk/aws-ecs');
+import lambda = require('@aws-cdk/aws-lambda');
+import { ApiEventSource } from '@aws-cdk/aws-lambda-event-sources';
 
 class HelloCDK extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.VpcNetwork(this, 'HelloVPC', { maxAZs: 2 });
-    const cluster = new ecs.Cluster(this, 'EC2Cluster', { vpc });
-    cluster.addCapacity('AutoScalingGroup', {
-      instanceType: new ec2.InstanceType('t2.micro')
+    const lambdaFn = new lambda.Function(this, 'HelloFunction', {
+      runtime: lambda.Runtime.NodeJS810,
+      handler: 'handler.handler',
+      code: lambda.Code.asset('./lambdaFn')
     });
 
-    const ecsService = new ecs.LoadBalancedEc2Service(this, 'EC2Service', {
-      cluster,
-      image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
-      memoryLimitMiB: 512
-    });
-
-    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
-      value: ecsService.loadBalancer.dnsName
-    });
+    const rule = new ApiEventSource('GET', '/');
+    rule.bind(lambdaFn);
   }
 }
 
 const app = new cdk.App();
 
-new HelloCDK(app, 'HelloCDK');
+new HelloCDK(app, 'HelloCDK', { env: { region: 'ap-northeast-2' } });
 
 app.run();
